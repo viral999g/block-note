060c00dd0a86c4ab3e532f0e5bf2b36dce9ee1a3
Viral <viral@wokelo.ai>
2023-09-25 19:41:42 +0530

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BlockNoteView, useBlockNote } from "@blocknote/react";
+import "@blocknote/core/style.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // Creates a new editor instance.
+  const editor = useBlockNote({
+    initialContent: [
+      {
+        id: "0e28a7c8-73d9-419b-a7cd-c388ee4bcb06",
+        type: "heading",
+        props: {
+          textColor: "default",
+          backgroundColor: "default",
+          textAlignment: "left",
+          level: "1",
+        },
+        content: [
+          {
+            type: "text",
+            text: "Wokelo123 ",
+            styles: {
+              textColor: "gray",
+            },
+          },
+          {
+            type: "text",
+            text: "Test ",
+            styles: {
+              bold: true,
+              italic: true,
+              textColor: "gray",
+            },
+          },
+        ],
+        children: [],
+      },
+      {
+        id: "85682f14-7cc9-4abd-9e2e-d0bc0e737f98",
+        type: "paragraph",
+        props: {
+          textColor: "default",
+          backgroundColor: "default",
+          textAlignment: "left",
+        },
+        content: [],
+        children: [],
+      },
+    ],
+    onEditorContentChange: (editor) => {
+      console.log(editor.topLevelBlocks);
+    },
+  });
+
+  // Renders the editor instance using a React component.
+  return <BlockNoteView editor={editor} />;
 }
 
 export default App;
